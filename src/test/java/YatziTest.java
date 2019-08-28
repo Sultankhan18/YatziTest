79af99363302964e0c1f8a3b6871cca15e094f41
Sultan <zohanwall@gmail.com>
2019-08-28 16:31:43 +0200

    firt commit

---
+++ src/test/java/YatziTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class YatziTest {
+
+    @Test
+    void shouldConvertToIV () {
+        assertEquals(4, 2+2);
+    }
+
+
+}
