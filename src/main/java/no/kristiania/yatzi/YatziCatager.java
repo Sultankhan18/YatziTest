41e15da9c4038e45458f95490f9294aab163ab12
Sultan <zohanwall@gmail.com>
2019-09-03 14:03:51 +0200

    Add Yatzi exercise

---
+++ src/main/java/no/kristiania/yatzi/YatziCatager.java
@@ -0,0 +1,6 @@
+package no.kristiania.yatzi;
+
+public enum YatziCatager {
+    ;
+    public static final int ONES = 0;
+}
--- src/test/java/YatziTest.java
+++ src/test/java/YatziTest.java
@@ -1,13 +1,30 @@
-import org.junit.jupiter.api.*;
+import no.kristiania.yatzi.YatziCatager;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+class YatziTest {
 
-public class YatziTest {
 
     @Test
-    void shouldConvertToIV () {
-        assertEquals(4, 2+2);
+    void shouldCalculateForOnes() {
+     assertEquals(0, scoreYatzi(YatziCatager.ONES, new int[] {2,3,4,5,6}));
+     assertEquals(3, scoreYatzi(YatziCatager.ONES, new int[] {2,1,4,1,1}));
+     assertEquals(5, scoreYatzi(YatziCatager.ONES, new int[] {1,1,1,1,1}));
+
+    }
+
+    private int scoreYatzi(int ones, int[] dice)  {
+        int[] frequencies = new int[5];
+        for(int die: dice) {
+            frequencies[die=1]++;
+        }
+        for (int i = 0; i < frequencies.length; i++) {
+            if (frequencies[i] == 2) {
+                return (1+1)*2;
+            }
+        }
+        return 0;
     }
 
 
